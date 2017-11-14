cd900c76f0879b440338e535ed5ae0d17d1d56c1
Victor Lopes <contato_victorlopes@hotmail.com>
2017-11-14 20:28:24 -0300

    layout dashboard modificado

--- app/Proprietario.php
+++ app/Proprietario.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Proprietario extends Model
+class Proprietario extends Funcionario
 {
     //
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    
+<div class="row" style="margin-left:300px; width:600px">
+<div class="col s12 m7">
+   
+    <div class="card horizontal">
+      <div class="card-image">
+        <img src="https://lorempixel.com/100/190/nature/6">
+      </div>
+      <div class="card-stacked">
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+            
 @endsection
