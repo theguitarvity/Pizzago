325db7a6cbe0ad6a079161be9864d55738c583d0
Matheus Fontes da Gama <matheus-fontes.gama@outlook.com>
2017-12-02 15:24:05 -0300

    Atributos classe Funcinario

--- app/Funcionario.php
+++ app/Funcionario.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Funcionario extends Model
 {
     //
+    protected $fillable = [
+        'nomeFuncionario', 'cpfFuncionario', 'enderecoFuncionario', 'telefoneFuncionario',
+    ];
 }
