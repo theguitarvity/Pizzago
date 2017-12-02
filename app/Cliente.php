9abbd41344ebf4c33af16a4259d135a3bb454039
Victor Lopes <contato_victorlopes@hotmail.com>
2017-12-02 15:35:10 -0300

    Estrutura das classes User e Cliente montada

--- app/Cliente.php
+++ app/Cliente.php
@@ -6,6 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends User
 {
+    protected $nomeCliente;
+    protected $enderecoCliente;
+    protected $telefoneCliente;
+
+    public function __construct($cod, $nome, $email, $senha, $endereco, $telefone){
+        parent::__construct($cod, $nome,$email, $senha);
+        $this->enderecoCliente = $endereco;
+        $this->telefoneCliente = $telefone;
+    }
+    public function getCodCliente(){
+        return parent::getCodUser();
+        
+    }
+    public function getNomeCliente(){
+        return parent::getNomeUser();
+    }
+    public function getEmailCliente(){
+        return parent::getEmailUser();
+    }
+    public function getSenhaCliente(){
+        return parent::getSenhaUser;
+    }
+    public function getEnderecoCliente(){
+        return $this->enderecoCliente;
+    }
+    public function getTelefoneCliente(){
+        return $this->telefoneCliente;
+    }
+
     //
     protected $fillable = [
         'codCliente','telefoneCliente', 'enderecoCliente',
--- app/User.php
+++ app/User.php
@@ -14,6 +14,32 @@ class User extends Authenticatable
      *
      * @var array
      */
+ 
+    protected $codUser;
+    protected $nomeUser;
+    protected $emaiUser;
+    protected $senhaUser;
+
+
+    public function __construct($cod, $nome, $email, $senha){
+        $this->codUser = $cod;
+        $this->nomeUser = $nome;
+        $this->emailUser = $emai;
+        $this->senha = $senha;
+    }
+
+    public function getCodUser(){
+        return $this->codUser;
+    }
+    public function getNomeUser(){
+        return $this->nomeUser;
+    }
+    public function getEmailUser(){
+        return $this->emailUser;
+    }
+    public function getSenhaUser(){
+        return $this->senhaUser;
+    }
     protected $fillable = [
        'id', 'nomeUsuario', 'email', 'password',
     ];
